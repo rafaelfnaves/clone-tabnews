1a05fee7c1f4b66da810d9bb4ec2e37f9063ecf2
Rafael Naves <r.naves21@gmail.com>
2025-11-26 23:01:57 -0300

    Adjust on endpoints to integrate with test database

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,14 +1,26 @@
-import migrationRunner from 'node-pg-migrate'
-import { join } from 'node:path'
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
 
-export default async function migrations(request, response) {
-  const migrations = await migrationRunner({
+const migrationRunnerOptions = (dryRun) => {
+  return {
     databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
+    dryRun: dryRun,
     dir: join("infra", "migrations"),
     direction: "up",
     migrationsTable: "pgmigrations",
     verbose: true,
-  })
+  };
+};
+
+export default async function migrations(request, response) {
+  let migrations = [];
+  if (request.method === "GET") {
+    migrations = await migrationRunner(migrationRunnerOptions(true));
+  } else if (request.method === "POST") {
+    migrations = await migrationRunner(migrationRunnerOptions(false));
+  } else {
+    return response.status(405).end();
+  }
+
   response.status(200).json(migrations);
 }
