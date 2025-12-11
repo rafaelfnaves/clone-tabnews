d6d8c149225afb43aea4efaba24e840c39e31809
Rafael Fernandes <rafael.fernandes@cilia.com.br>
2025-12-11 12:56:15 -0300

    feat: refactor migrations controller

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,26 +1,35 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
-
-const defaultMigrationOptions = {
-  databaseUrl: process.env.DATABASE_URL,
-  dir: join("infra", "migrations"),
-  direction: "up",
-  migrationsTable: "pgmigrations",
-  verbose: true,
-  dryRun: true,
-};
+import database from "infra/database";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    migrationsTable: "pgmigrations",
+    verbose: true,
+    dryRun: true,
+  };
+
   if (request.method === "GET") {
     const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+
+    await dbClient.end();
+
     return response.status(200).json(pendingMigrations);
   }
-  
+
   if (request.method === "POST") {
     const migratedMigrations = await migrationRunner({
       ...defaultMigrationOptions,
       dryRun: false,
     });
+
+    await dbClient.end();
+
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
     }
